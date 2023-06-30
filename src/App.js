18db73644c61f94afaac2a4d0bf89a7c0ae8e02d
nhillson <nicholas.hillson@gmail.com>
2023-06-30 12:04:42 -0500

    Designed basic UI for entry page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const lifts = [
+  "Bent Row",
+  "Bicep Curl",
+  "Chest Fly",
+  "Machine Press",
+  "Chin Up",
+  "Deadlift",
+  "Face Pull",
+  "Front Raise"
+]
+
+function SearchBar() {
+  return (
+    <input type="text" placeholder="search..." />
+  );
+}
+function LiftRow({ lift }) {
+  return (
+    <tr>
+        <td>{lift}</td>
+        <td><input type="date" placeholder="date" /></td>
+        <td><input type="number" placeholder="weight (lbs)" /></td>
+    </tr>
+  );
+}
+
+function LiftTable({ lifts }) {
+  const rows = [];
+  lifts.forEach((lift) => {
+    rows.push(<LiftRow lift={lift} />)
+  }
+  )
+
+  return (
+    <div className="liftTable">
+      <table className="liftTable">
+        <thead>
+          <tr>
+            <th>Lift</th>
+            <th>Date</th>
+            <th>Weight (lbs)</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+      <button onClick={() => alert('clicked!')}>Submit</button>
+    </div>
+    
+  );
+}
+function FilterableTable({ lifts }) {
+  return (
+    <>
+      <SearchBar />
+      <LiftTable lifts={lifts}/>
+    </>
+  );
+}
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lift Tracker</h1>
+      <FilterableTable lifts={lifts} />
     </div>
   );
 }
 
-export default App;
+
